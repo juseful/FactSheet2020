d886f87d783e08c90da8bf4a2eed678ac0acf92d
juseful <juseful.lee@gmail.com>
2021-12-01 09:49:08 +0900

    Create PDF files in order of image list.

--- result_img_merge_to_pdf.py
+++ result_img_merge_to_pdf.py
@@ -11,15 +11,12 @@ def print_files_in_dir(root_dir, prefix):
     files = os.listdir(root_dir)
     for file in files:
         path = os.path.join(root_dir, file)
-#         print(prefix + path)
         if os.path.isdir(path):
             print_files_in_dir(path, prefix + "    ")
-#         print(path)
         filedir.append(path) # 하위폴더의 세부 파일경로를 리스트에 저장
 
 if __name__ == "__main__":
     root_dir = workdir
-    # root_dir = "W:/PWV_deID_IMAGE/CASE_TEST/TEST6-1"
     print_files_in_dir(root_dir, "")
 
 filelist = [filedir for filedir in filedir if '.PNG' in filedir.upper()]
@@ -29,11 +26,6 @@ filelist
 #%%
 img_list = []
 
-# 1st page set
-img_path = filelist[0]
-im_buf = Image.open(img_path)
-cvt_rgb_0 = im_buf.convert('RGB')
-
 for file in filelist:
     im_buf = Image.open(file)
     cvt_rgb = im_buf.convert('RGB')
@@ -42,6 +34,5 @@ for file in filelist:
 img_list
 
 # %%
-del img_list[0]
-cvt_rgb_0.save('{}/test.pdf'.format(workdir),save_all=True, append_images=img_list)
-# %%
+# in the order of image list
+img_list[0].save('{}/test_21120102.pdf'.format(workdir),save_all=True, append_images=img_list[1:])
